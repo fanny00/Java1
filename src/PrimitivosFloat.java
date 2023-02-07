c5f983de6e7f87f437d8e503eb322951d136f6f3
Fanny Campos <fannycamposa@gmail.com>
2023-02-07 13:18:47 -0300

    Primitivos numericos de punto flotante

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,10 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 2.12e3f; //2120f;
+        System.out.println("realFloat = " + realFloat);
+        
+        float realFloatNegativo = 1.5e-10f; // 0.00000000015f
+        System.out.println("realFloatNegativo = " + realFloatNegativo);
+    }
+}
