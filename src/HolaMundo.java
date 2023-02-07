4d39c19a9ec4ccd95aa5166d6b6b7c82196d68d2
Fanny Campos <fannycamposa@gmail.com>
2023-02-07 12:01:04 -0300

    variables

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -19,5 +19,18 @@ public class HolaMundo {
         System.out.println("numero2 = " + numero2);
 
         var numero3 = "15";
+
+        String nombre;
+
+        nombre = "Andres";
+        
+        if( numero > 10 ){
+            nombre = "Juan";
+        }
+
+        System.out.println("nombre = " + nombre);
+
+        int edadPersona = 5;
+
     }
 }
